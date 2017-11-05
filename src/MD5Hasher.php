040ca00a0c846c2bab747d9657ce0ad6d5c41993
vagrant <vagrant@homestead>
2017-11-05 10:38:39 +0000

    init project

---
+++ src/MD5Hasher.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Dengkangfa\Hasher;
+
+/**
+ * Class MD5Hasher
+ * @package Dengkangfa\Hasher
+ */
+class MD5Hasher
+{
+    /**
+     * @param $value
+     * @param array $options
+     * @return bool
+     */
+    public function make($value, array $options = []) : string
+    {
+        $salt = $options['salt'] ?? '';
+        return hash('md5', $value . $salt);
+    }
+
+    /**
+     * @param $value
+     * @param $hashValue
+     * @param array $options
+     * @return bool
+     */
+    public function check($value, $hashValue, array $options = []) : bool
+    {
+        $salt = $options['salt'] ?? '';
+        return hash('md5', $value . $salt) === $hashValue;
+    }
+}
